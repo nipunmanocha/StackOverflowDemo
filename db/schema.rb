71d5907c0710a38d45e5e2c68b738add3d127745
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-05 13:35:32 +0530

    migrated table answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104152505) do
+ActiveRecord::Schema.define(version: 20180105065957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "text", null: false
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.boolean "accepted", default: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
@@ -36,5 +48,7 @@ ActiveRecord::Schema.define(version: 20180104152505) do
     t.index ["salt"], name: "index_users_on_salt"
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
