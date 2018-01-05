dc9a10af035360a35c885e35e6c9a9bb4ef23ed7
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-05 13:14:20 +0530

    rollback db

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105065957) do
+ActiveRecord::Schema.define(version: 20180104152505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "answers", force: :cascade do |t|
-    t.string "text", null: false
-    t.integer "question_id", null: false
-    t.integer "user_id", null: false
-    t.boolean "accepted", default: false
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
-  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
@@ -48,7 +36,5 @@ ActiveRecord::Schema.define(version: 20180105065957) do
     t.index ["salt"], name: "index_users_on_salt"
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
