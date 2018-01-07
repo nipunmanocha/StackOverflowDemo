8ec10079c750a54fb60de19e8dfc7be30b1e38a6
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-07 17:20:00 +0530

    created user sessions

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',        '3.1.11'
+gem 'jquery-rails', '4.3.1' 
 
 
 group :development, :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Stack Overflow!"
+      log_in @user
       redirect_to @user
     else
       render 'new'
