ad51f6db880d63950237d399467a15af4e07c451
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-12 11:32:53 +0530

    working question specs

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -2,7 +2,10 @@ module ErrorHandlers
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound, with: { head: :not_found }
+    rescue_from ActiveRecord::RecordNotFound do
+      head :not_found
+    end
+    
     rescue_from ActiveRecord::RecordInvalid do |exception|
       render json: exception, status: :bad_request 
     end
--- spec/controller/v1/questions_controller_spec.rb
+++ spec/controller/v1/questions_controller_spec.rb
@@ -52,57 +52,58 @@ RSpec.describe V1::QuestionsController, type: :controller do
   end
 
   describe 'PUT #update' do
-    # context 'non logged in user' do
-    #   it 'should redirect to signup path' do
+    context 'non logged in user' do
+      it 'should redirect to signup path' do
+        question.save
+        put :update, params: { id: question.id, question: { text: 'updated' } }
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to have_http_status(:unauthorized)
+        expect(parsed_body[:redirect_url]).to eq(v1_signup_path)
+      end
+    end
 
-    #     # put :cr, params: { user_id: user[:id], question: { text: question[:text] } }
-    #     parsed_body = JSON.parse(response.body, symbolize_names: true)
-    #     expect(response).to have_http_status(:unauthorized)
-    #     expect(parsed_body[:redirect_url]).to eq(v1_signup_path)
-    #   end
-    # end
+    context 'logged in user' do
+      it 'should not update if question is not present' do 
+        question.save
+        session[:user_id] = question.user.id
+        Question.delete_all
+        put :update, params: { id: question[:id], question: { text: 'Is this updated question?' } }
+        expect(response).to have_http_status :not_found
+      end
 
-    # context 'logged in user' do
-    #   it 'should not update if question is not present' do 
-    #     question.save
-    #     Question.delete_all
-    #     put :update, params: { id: question[:id], question: { text: 'Is this updated question?' } }
-    #     expect(response).to have_http_status :not_found
-    #   end
+      it 'should not update if text is empty' do
+        question.save
+        session[:user_id] = question.user.id
+        put :update, params: { id: question[:id], question: { text: '' } }
+        expect(response).to have_http_status(:bad_request)
+      end
 
-    #   it 'should not update if text is empty' do
-    #     question.save
-    #     session[:user_id] = question.user.id
-    #     put :update, params: { id: question[:id], question: { text: '' } }
-    #     expect(response).to have_http_status(:unprocessable_entity)
-    #   end
-
-    #   it 'should update if all valid params' do
-    #     question.save
-    #     session[:user_id] = question.user.id
-    #     updated_text = 'Is this the updated question?'
-    #     put :update, params: { id: question[:id], question: { text: updated_text } }
-    #     expect(response).to have_http_status(:ok)
-    #     expect(Question.find(question[:id])[:text]).to eq(updated_text)
-    #   end
-    # end
+      it 'should update if all valid params' do
+        question.save
+        session[:user_id] = question.user.id
+        updated_text = 'Is this the updated question?'
+        put :update, params: { id: question[:id], question: { text: updated_text } }
+        expect(response).to have_http_status(:ok)
+        expect(Question.find(question[:id])[:text]).to eq(updated_text)
+      end
+    end
   end
 
-  # describe 'DELETE #destroy' do
-  #   it 'should not delete if question does not belong to logged in user' do
-  #     question.save
-  #     new_user = FactoryBot.create(:user)
-  #     session[:user_id] = new_user.id
-  #     delete :destroy, params: { id: question[:id] }
-  #     expect(response).to have_http_status :not_found
-  #   end
+  describe 'DELETE #destroy' do
+    it 'should not delete if question does not belong to logged in user' do
+      question.save
+      new_user = FactoryBot.create(:user)
+      session[:user_id] = new_user.id
+      delete :destroy, params: { id: question[:id] }
+      expect(response).to have_http_status :not_found
+    end
 
-  #   it 'should delete if question belong to logged in user' do
-  #     question.save
-  #     session[:user_id] = question.user.id
-  #     delete :destroy, params: { id: question[:id] }
-  #     expect(response).to have_http_status(:ok)
-  #     expect(Question.find_by(id: question[:id])).to be_nil
-  #   end
-  # end
+    it 'should delete if question belong to logged in user' do
+      question.save
+      session[:user_id] = question.user.id
+      delete :destroy, params: { id: question[:id] }
+      expect(response).to have_http_status(:ok)
+      expect(Question.find_by(id: question[:id])).to be_nil
+    end
+  end
 end
