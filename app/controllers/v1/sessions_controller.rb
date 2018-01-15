afd85ac320d7f2714cf8caa9fc7b38445c9ab224
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-15 18:36:13 +0530

    added cookies concept

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -6,6 +6,7 @@ class V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       render json: user, status: :ok
     else
       render json: { error: "Incorrect Username/Password combination" }, status: 400
@@ -13,7 +14,7 @@ class V1::SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     render json: { message: "Successfully Logged Out" }, status: 201
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105161532) do
+ActiveRecord::Schema.define(version: 20180115122525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20180105161532) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
