7451ced84dd77eaf6e880eed23ff2d32a20ec59c
Nipun Manocha <nipun.manocha@1mg.com>
2018-01-15 17:37:57 +0530

    inendation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,22 @@
 #
 
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-    has_secure_password
-    preserve_data
+  has_secure_password
+  preserve_data
 
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :email, 
-        presence: true, 
-        length: { maximum: 255 }, 
-        format: { with: VALID_EMAIL_REGEX },
-        uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 
-    has_many :questions
-    has_many :answers
-    has_many :votes
-    has_many :comments
+  has_many :questions
+  has_many :answers
+  has_many :votes
+  has_many :comments
 
-    before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
 end
